e83360120fe6367ab73a4840776868331abd53c3
HP <HP@LAPTOP-OK396O9K>
2019-09-17 11:19:54 +0530

    Successfully completed

--- src/main/java/main/ToughQuestion2.java
+++ src/main/java/main/ToughQuestion2.java
@@ -18,38 +18,54 @@ class ToughQuestion2 {
                         switch(choice)
                         {
                         case 1 : System.out.println("Enter the number of Slice of bread");
-                        	bread = Integer.parseInt(br.readLine());
-                        	//call the function which calculate the calories for only bread
-                        	System.out.println();
-                        	break;
+                        bread = Integer.parseInt(br.readLine());
+                        System.out.printf("%.3f",cal.calculaeEnergy(cal.calculateCalories(bread)));
+                        //call the function which calculate the calories for only bread
+                        System.out.println("kJ of energy generated from " + cal.returnCalories()+"calories");
+                        break;
                         case 2 : System.out.println("Enter the number of Slice of bread");
-                        	bread = Integer.parseInt(br.readLine());
-                        	System.out.println("Enter the number teaspoon of Jam");
-                        	jam = Integer.parseInt(br.readLine());
-                        	//call the function which calculate the calories for only bread & jam
-                        	System.out.println();
-                        	break;
+                        bread = Integer.parseInt(br.readLine());
+                        System.out.println("Enter the number teaspoon of Jam");
+                        jam = Integer.parseInt(br.readLine());
+                        System.out.printf("%.3f",cal.calculaeEnergy(cal.calculateCalories(bread , jam)));
+                        //call the function which calculate the calories for only bread & jam
+                        System.out.println("kJ of energy generated from " + cal.returnCalories()+"calories");
+                        break;
                         case 3 : System.out.println("Enter the number of Slice of bread");
-                        	bread = Integer.parseInt(br.readLine());
-                        	System.out.println("Enter the number teaspoon of Jam");
-                        	jam = Integer.parseInt(br.readLine());
-                        	System.out.println("Enter the number teaspoon of Butter");
-                        	butter = Integer.parseInt(br.readLine());
-                        	//call the function which calculate the calories for  bread,jam & butter
-                        	System.out.println();
-                        	break;
+                        bread = Integer.parseInt(br.readLine());
+                        System.out.println("Enter the number teaspoon of Jam");
+                        jam = Integer.parseInt(br.readLine());
+                        System.out.println("Enter the number teaspoon of Butter");
+                        butter = Integer.parseInt(br.readLine());
+                        //call the function which calculate the calories for  bread,jam & butter
+                        System.out.printf("%.3f",cal.calculaeEnergy(cal.calculateCalories(bread , jam , butter)));
+                        System.out.println("kJ of energy generated from " + cal.returnCalories()+"calories");
+                        break;
                         }
                         //System.out.printf("%.3f",ca.calculateEnergy());
                         //System.out.println(" kJ of energy generated from "+ca.returnCalories()+" calories");
             }
 }
 class Calories {
-            
-            int calculateCalories() {
+           
+private int calories;
+            public int calculateCalories(int nBread) {
+            calories = 74 * nBread;
             //you have to overload this function for three parameters refer the question
-            	return -1;
+            return calories;
             }
-            
-            
-            
+            public int calculateCalories(int nBread, int nJam) {
+            calories=74 * nBread + 26 * nJam;
+            return calories;
+            }
+            public int calculateCalories(int nBread,int nJam,int nButter) {
+            calories=74 * nBread + 26 * nJam + 102 * nButter;
+            return calories;
+            }
+            public double calculateEnergy(int calorie) {
+            return calorie*4.1868/1000;
+            }
+            public int returnCalories() {
+            return calories;
+            }
 }
